2765e8f0df6844d60a164c5792651c4bd850273c
raineduc <hramil0120@gmail.com>
2022-11-06 18:46:04 +0300

    Fix mpf monolith implementation with regular recursion

--- src/max-prime-factor/mpf_monolith.erl
+++ src/max-prime-factor/mpf_monolith.erl
@@ -14,24 +14,22 @@
 
 find_mpf(2) -> 2;
 find_mpf(3) -> 3;
-find_mpf(N) when N > 3 -> find_max_prime_factor(N, 2, -1).
+find_mpf(N) when N > 3 -> find_max_prime_factor(N, 2).
 
 
-find_max_prime_factor(RestNumber, PrimeFactor, _) when PrimeFactor * PrimeFactor > RestNumber, RestNumber > 1 ->
+find_max_prime_factor(RestNumber, PrimeFactor) when PrimeFactor * PrimeFactor > RestNumber, RestNumber > 1 ->
   RestNumber;
-
-find_max_prime_factor(RestNumber, PrimeFactor, MaxFactor) when RestNumber > 1 ->
-  {NewRest, NewMaxFactor} =
-    if RestNumber rem PrimeFactor == 0 ->
-      {sieve_prime_factor(RestNumber, PrimeFactor), max(PrimeFactor, MaxFactor)};
-      true -> {RestNumber, MaxFactor}
-    end,
-  find_max_prime_factor(NewRest, PrimeFactor + 1, NewMaxFactor);
-
-find_max_prime_factor(_, _, MaxFactor) -> MaxFactor.
-
+find_max_prime_factor(RestNumber, PrimeFactor) when RestNumber > 1 ->
+  NewRest = sieve_prime_factor(RestNumber, PrimeFactor),
+  MaxFactor = find_max_prime_factor(NewRest, PrimeFactor + 1),
+  if
+    RestNumber rem PrimeFactor == 0 ->
+      max(PrimeFactor, MaxFactor);
+    true -> MaxFactor
+  end;
+find_max_prime_factor(_, _) -> 0.
 
 sieve_prime_factor(RestNumber, PrimeFactor) when RestNumber rem PrimeFactor == 0 ->
   sieve_prime_factor(RestNumber div PrimeFactor, PrimeFactor);
-
 sieve_prime_factor(RestNumber, _) -> RestNumber.
+
--- src/max-prime-factor/mpf_monolith_tailed.erl
+++ src/max-prime-factor/mpf_monolith_tailed.erl
@@ -19,7 +19,6 @@ find_mpf(N) when N > 3 -> find_max_prime_factor(N, 2, -1).
 
 find_max_prime_factor(RestNumber, PrimeFactor, _) when PrimeFactor * PrimeFactor > RestNumber, RestNumber > 1 ->
   RestNumber;
-
 find_max_prime_factor(RestNumber, PrimeFactor, MaxFactor) when RestNumber > 1 ->
   {NewRest, NewMaxFactor} =
     if RestNumber rem PrimeFactor == 0 ->
@@ -27,11 +26,9 @@ find_max_prime_factor(RestNumber, PrimeFactor, MaxFactor) when RestNumber > 1 ->
       true -> {RestNumber, MaxFactor}
     end,
   find_max_prime_factor(NewRest, PrimeFactor + 1, NewMaxFactor);
-
 find_max_prime_factor(_, _, MaxFactor) -> MaxFactor.
 
 
 sieve_prime_factor(RestNumber, PrimeFactor) when RestNumber rem PrimeFactor == 0 ->
   sieve_prime_factor(RestNumber div PrimeFactor, PrimeFactor);
-
 sieve_prime_factor(RestNumber, _) -> RestNumber.
